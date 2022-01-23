e7d204d1c6cc3a91879f400e698fa93091d993f5
mochi <mirage_mc@icloud.com>
2022-01-23 16:01:19 +0800

    登陆验证处理

--- src/main/java/com/mmcc/admin/controller/IndexController.java
+++ src/main/java/com/mmcc/admin/controller/IndexController.java
@@ -27,7 +27,7 @@ public class IndexController {
             //登陆成功，重定向到main.html,能够防止表单重复提交
             return "redirect:/main.html";
         }else {
-            model.addAttribute("msg","账号密码错误");
+            model.addAttribute("msg","请输入账号密码");
             //回到登陆页面
             return "login";
         }
@@ -36,11 +36,14 @@ public class IndexController {
 
 //    为了解决重复提交问题，但是未登录情况下不能直接对此页面请求
     @GetMapping("/main.html")
-    public String mainPage(HttpSession  session){
+    public String mainPage(HttpSession  session,Model model){
         //判断用户是否已经登录，可使用拦截器与过滤器机制
         Object loginUser=session.getAttribute("loginUser");//从session中获取loginUser字段
-        if(loginUser==null)
+        if(loginUser==null) {
+            //回到登陆页面
+            model.addAttribute("msg","服务器Session失效");
             return "login";
+        }
         else
             return "main";
     }
