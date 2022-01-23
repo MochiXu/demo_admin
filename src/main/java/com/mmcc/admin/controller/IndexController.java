653fdee260b5a0b2ea4d2b326fe4440289f1e168
mochi <mirage_mc@icloud.com>
2022-01-23 15:44:33 +0800

    initial

---
+++ src/main/java/com/mmcc/admin/controller/IndexController.java
@@ -0,0 +1,47 @@
+package com.mmcc.admin.controller;
+
+import com.mmcc.admin.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class IndexController {
+    /**
+     * 访问登录页
+     * */
+    @GetMapping(value={"/","/login"})
+    public String loginPage(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String main(User user, HttpSession httpSession, Model model){
+        if(!StringUtils.isEmpty(user.getUserName())&&!StringUtils.isEmpty(user.getPassword())){
+            //只要是有值，那就视为登陆成功
+            httpSession.setAttribute("loginUser",user);
+            //登陆成功，重定向到main.html,能够防止表单重复提交
+            return "redirect:/main.html";
+        }else {
+            model.addAttribute("msg","账号密码错误");
+            //回到登陆页面
+            return "login";
+        }
+
+    }
+
+//    为了解决重复提交问题，但是未登录情况下不能直接对此页面请求
+    @GetMapping("/main.html")
+    public String mainPage(HttpSession  session){
+        //判断用户是否已经登录，可使用拦截器与过滤器机制
+        Object loginUser=session.getAttribute("loginUser");//从session中获取loginUser字段
+        if(loginUser==null)
+            return "login";
+        else
+            return "main";
+    }
+}
